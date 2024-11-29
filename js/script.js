df13faaa8f46399b8de95ff490a0033ace9d7bb5
zahahyang <sarah20120829@gmail.com>
2024-11-29 17:18:09 +0800

    1129

--- js/script.js
+++ js/script.js
@@ -37,6 +37,41 @@
 
 		});
 	}
+// init Isotope
+	var initIsotope = function () {
+
+		$('.grid').each(function () {
+
+			// $('.grid').imagesLoaded( function() {
+			// images have loaded
+			var $buttonGroup = $('.button-group');
+			var $checked = $buttonGroup.find('.is-checked');
+			var filterValue = $checked.attr('data-filter');
+
+			var $grid = $('.grid').isotope({
+				itemSelector: '.portfolio-item',
+				// layoutMode: 'fitRows',
+				filter: filterValue
+			});
+
+			// bind filter button click
+			$('.button-group').on('click', 'a', function (e) {
+				e.preventDefault();
+				filterValue = $(this).attr('data-filter');
+				$grid.isotope({ filter: filterValue });
+			});
+
+			// change is-checked class on buttons
+			$('.button-group').each(function (i, buttonGroup) {
+				$buttonGroup.on('click', 'a', function () {
+					$buttonGroup.find('.is-checked').removeClass('is-checked');
+					$(this).addClass('is-checked');
+				});
+			});
+			// });
+
+		});
+	}
 
   var initTexts = function(){
     // Wrap every letter in a span
