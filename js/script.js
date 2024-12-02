810c82ae2359605e06263d6bfe2b25a937e3dbdf
zahahyang <sarah20120829@gmail.com>
2024-12-02 16:55:18 +0800

    1202-2

--- js/script.js
+++ js/script.js
@@ -37,41 +37,7 @@
 
 		});
 	}
-// init news
-	var initIsotope = function () {
 
-		$('.grid').each(function () {
-
-			// $('.grid').imagesLoaded( function() {
-			// images have loaded
-			var $buttonGroup = $('.button-group');
-			var $checked = $buttonGroup.find('.is-checked');
-			var filterValue = $checked.attr('data-filter');
-
-			var $grid = $('.grid').isotope({
-				itemSelector: '.news-item',
-				// layoutMode: 'fitRows',
-				filter: filterValue
-			});
-
-			// bind filter button click
-			$('.button-group').on('click', 'a', function (e) {
-				e.preventDefault();
-				filterValue = $(this).attr('data-filter');
-				$grid.isotope({ filter: filterValue });
-			});
-
-			// change is-checked class on buttons
-			$('.button-group').each(function (i, buttonGroup) {
-				$buttonGroup.on('click', 'a', function () {
-					$buttonGroup.find('.is-checked').removeClass('is-checked');
-					$(this).addClass('is-checked');
-				});
-			});
-			// });
-
-		});
-	}
 
   var initTexts = function(){
     // Wrap every letter in a span
