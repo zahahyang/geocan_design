e597e90ea05c2cda781ecb8512754020b64591b8
zahahyang <sarah20120829@gmail.com>
2024-12-05 11:41:13 +0800

    1205

--- js/script.js
+++ js/script.js
@@ -164,6 +164,7 @@
     });
   }
 
+
   //Overlay Menu Navigation
   var overlayMenu = function () {
 
@@ -223,3 +224,22 @@
 	});
 
 })(jQuery);
+
+ /**
+   * Init swiper sliders
+   */
+ function initSwiper() {
+  document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+    let config = JSON.parse(
+      swiperElement.querySelector(".swiper-config").innerHTML.trim()
+    );
+
+    if (swiperElement.classList.contains("swiper-tab")) {
+      initSwiperWithCustomPagination(swiperElement, config);
+    } else {
+      new Swiper(swiperElement, config);
+    }
+  });
+}
+
+window.addEventListener("load", initSwiper);
